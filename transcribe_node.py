c7c089f8077bf02618ea83b24937ae3d779d4ea6
Quoc Nguyen <53263073+minhquoc0712@users.noreply.github.com>
2023-06-21 11:54:13 +0300

    Update transcribe_node.py

--- transcribe_node.py
+++ transcribe_node.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python 
+#! /usr/bin/env python
 
 import rospy
 from audio_common_msgs.msg import AudioData
@@ -14,6 +14,7 @@ import os
 import torch
 import whisper
 import time
+import wave
 import argparse
 import json
 from vosk import Model, KaldiRecognizer
@@ -22,21 +23,24 @@ from opendr.perception.speech_transcription import (
     WhisperLearner,
     VoskLearner,
 )
+from opendr_bridge import ROSBridge
+from opendr_bridge.msg import OpenDRTranscription
+from opendr.engine.target import VoskTranscription
 
 
-class AudioProcessor:
+class TranscriptionNode:
     def __init__(self, args):
-        rospy.init_node('audio_processor')
+        rospy.init_node("opendr_transcription_node")
 
         self.data_queue = Queue()
 
-        # Initialize model
         self.backbone = args.backbone
         self.model_name = args.model_name
         self.model_path = args.model_path
-        self.language = None if args.language.lower() == "none" else args.language 
+        self.language = None if args.language.lower() == "none" else args.language
         self.download_dir = args.download_dir
 
+        # Initialize model
         if self.backbone == "whisper":
             if self.model_path is not None:
                 name = self.model_path
@@ -48,14 +52,24 @@ class AudioProcessor:
         else:
             # Load Vosk model
             self.audio_model = VoskLearner()
-            self.audio_model.load(name=self.model_name, model_path=self.model_path, language=self.language, download_dir=self.download_dir)
+            self.audio_model.load(
+                name=self.model_name,
+                model_path=self.model_path,
+                language=self.language,
+                download_dir=self.download_dir,
+            )
 
-        self.last_sample = b''
+        self.last_sample = b""
+        self.cut_audio = False
 
         self.temp_file = args.temp_file
-        self.transcription = ['']
 
-        self.sub = rospy.Subscriber(args.input_topic, AudioData, self.callback)
+        self.subscriber = rospy.Subscriber(args.input_topic, AudioData, self.callback)
+        self.publisher = rospy.Publisher(
+            args.output_topic, OpenDRTranscription, queue_size=10
+        )
+
+        self.bridge = ROSBridge()
 
         # Start processing thread
         self.processing_thread = Thread(target=self.process_audio)
@@ -76,73 +90,115 @@ class AudioProcessor:
             if not self.data_queue.empty():
                 # Get the audio data from the queue
                 # Concatenate our current audio data with the latest audio data.
+                new_data = b""
                 while not self.data_queue.empty():
-                    data = self.data_queue.get()
-                    self.last_sample += data
+                    new_data += self.data_queue.get()
+
+                # print(self.cut_audio)
+                if self.backbone == "vosk" and self.cut_audio:
+                    self.last_sample = new_data
+                    self.cut_audio = False
+                else:
+                    self.last_sample += new_data
 
                 # Write wav data to the temporary file.
-                with wave.open(self.temp_file, 'wb') as f:
+                with wave.open(self.temp_file, "wb") as f:
                     f.setnchannels(1)
-                    f.setsampwidth(self.sample_width) 
+                    f.setsampwidth(self.sample_width)
                     f.setframerate(self.framerate)
                     # Convert audio data to numpy array
                     numpy_data = np.frombuffer(self.last_sample, dtype=np.int16)
-                    if self.n_sample is not None:
-                        numpy_data = numpy_data[(self.n_sample - 3200):]
-                        self.n_sample = None
+                    if self.backbone == "whisper" and self.cut_audio:
+                        if self.n_sample is not None:
+                            if self.n_sample < numpy_data.shape[0]:
+                                if self.n_sample >= 3200:
+                                    numpy_data = numpy_data[(self.n_sample - 3200) :]
+                                else:
+                                    numpy_data = numpy_data[(self.n_sample) :]
+                                self.last_sample = self.last_sample[(self.n_sample * 2) :]
+                                self.n_sample = None
+                                print("--------------------------")
+
+                        self.cut_audio = False
 
                     f.writeframes(numpy_data.tobytes())
 
                 # Process audio
                 if self.backbone == "vosk":
-                    with open(self.temp_file, 'rb') as f:
-                        while True:
-                            data = f.read(4000)
-                            if len(data) == 0:
-                                break
-                            # if self.recognizer.AcceptWaveform(data):
-                            #     result = json.loads(self.recognizer.Result())
-                            #     text = result['text'].strip()
-                            # else:
-                            #     result = json.loads(self.recognizer.PartialResult())
-                            #     text = result['partial'].strip()
-                            result = self.audio_model.infer(data)
-                            text = text.data
+                    wf = wave.open(self.temp_file, "rb")
+                    while True:
+                        data = wf.readframes(4000)
+                        if len(data) == 0:
+                            break
 
-                    # if text != '':
-                    #     if phrase_complete:
-                    #         self.transcription.append(text)
-                    #     else:
-                    #         self.transcription[-1] = text
-                    if text == '' and self.transcription[-1] != '':
-                        self.transcription.append('')
-                    else:
-                        self.transcription[-1] = text
+                        # print(len(data))
+                        # if len(data) < 7000:
+                        #     break
 
-                    # print(text)
-                    os.system('cls' if os.name=='nt' else 'clear')
-                    # print(len(self.transcription))
-                    print(self.transcription)
-                    for line in self.transcription:
-                        print(line)
+                        transcription = self.audio_model.infer(data)
+
+                        if transcription.accept_waveform:
+                            print(f"Text: {transcription.data}")
+                            self.cut_audio = True
+
+                            ros_transcription = self.bridge.to_ros_transcription(
+                                transcription
+                            )
+                            self.publisher.publish(ros_transcription)
+                        else:
+                            print(f"Partial: {transcription.data}")
 
                 else:
                     audio_array = whisper.load_audio(self.temp_file)
-                    transcription_whisper = self.audio_model.infer(audio_array, builtin_transcribe=True)
+                    phrase_timeout = 2  # Seconds
+                    if audio_array.shape[0] > phrase_timeout * self.framerate:
+                        t = self.audio_model.infer(
+                            audio_array[-phrase_timeout * self.framerate :]
+                        )
+                        if t.data == "" or t.segments[-1]["no_speech_prob"] > 0.5:
+                            self.cut_audio = True
+                    transcription_whisper = self.audio_model.infer(
+                        audio_array, builtin_transcribe=True
+                    )
                     segments = transcription_whisper.segments
-                    if len(segments) > 1:
+                    if len(segments) > 1 and segments[-1]["text"] != "":
                         last_segment = segments[-1]
-                        start_timestamp = last_segment['start']
+                        start_timestamp = last_segment["start"]
                         self.n_sample = int(self.framerate * start_timestamp)
+                        self.cut_audio = True
 
-                        text = [segments[i]['text'].strip() for i in range(len(segments) - 1)]
+                        text = [
+                            segments[i]["text"].strip()
+                            for i in range(len(segments) - 1)
+                        ]
                         text = " ".join(text)
-                    else:   
+                        transcription = VoskTranscription(
+                            text=text, accept_waveform=True
+                        )
+                    elif self.cut_audio:
+                        self.n_sample = audio_array.shape[0]
                         text = transcription_whisper.data.strip()
+                        transcription = VoskTranscription(
+                            text=text, accept_waveform=True
+                        )
+                        ros_transcription = self.bridge.to_ros_transcription(
+                            transcription
+                        )
+                        self.publisher.publish(ros_transcription)
+                    else:
+                        text = transcription_whisper.data.strip()
+                        transcription = VoskTranscription(
+                            text=text, accept_waveform=False
+                        )
 
-                    os.system('cls' if os.name=='nt' else 'clear')
+                    ros_transcription = self.bridge.to_ros_transcription(
+                        transcription
+                    )
+                    self.publisher.publish(ros_transcription)
+
+                    # os.system('cls' if os.name=='nt' else 'clear')
                     if self.n_sample is not None:
-                        print(last_segment['text'])
+                        print(text)
                     else:
                         print(text)
 
@@ -152,23 +208,48 @@ class AudioProcessor:
     def spin(self):
         rospy.spin()
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Process some integers.')
-    parser.add_argument('--backbone', default='whisper', help='backbone to use for audio processing. Options: whisper, vosk', choices=['whisper', 'vosk'])
-    parser.add_argument('--model-name', default='tiny', help='model to use for audio processing. Options: tiny, small, medium, large, en-us')
-    parser.add_argument('--model-path', default=None, help='path to model')
-    parser.add_argument('--download-dir', default=None, help='directory to download models to')
-    parser.add_argument('--language', default="en", help='language to use for audio processing')
-    parser.add_argument('--temp-file', default='./temp.wav', help='temporary file foraudio data')
-    parser.add_argument('--input-topic', default='/audio/audio', help='name of the topic to subscribe')
-    parser.add_argument('--output-topic', default='/audio/transcription', help='name of the topic to publish')
-    parser.add_argument('--sample-width', type=int, default=2, help='sample width for audio data')
-    parser.add_argument('--framerate', type=int, default=16000, help='framerate for audio data')
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Process some integers.")
+    parser.add_argument(
+        "--backbone",
+        default="whisper",
+        help="backbone to use for audio processing. Options: whisper, vosk",
+        choices=["whisper", "vosk"],
+    )
+    parser.add_argument(
+        "--model-name",
+        default=None,
+        help="model to use for audio processing. Options: tiny, small, medium, large",
+    )
+    parser.add_argument("--model-path", default=None, help="path to model")
+    parser.add_argument(
+        "--download-dir", default=None, help="directory to download models to"
+    )
+    parser.add_argument(
+        "--language", default="en", help="language to use for audio processing"
+    )
+    parser.add_argument(
+        "--temp-file", default="./temp.wav", help="temporary file foraudio data"
+    )
+    parser.add_argument(
+        "--input-topic", default="/audio/audio", help="name of the topic to subscribe"
+    )
+    parser.add_argument(
+        "--output-topic",
+        default="/audio/transcription",
+        help="name of the topic to publish",
+    )
+    parser.add_argument(
+        "--sample-width", type=int, default=2, help="sample width for audio data"
+    )
+    parser.add_argument(
+        "--framerate", type=int, default=16000, help="framerate for audio data"
+    )
     args = parser.parse_args()
 
     try:
-        node = AudioProcessor(args)
+        node = TranscriptionNode(args)
         node.spin()
     except rospy.ROSInterruptException:
         pass
-
